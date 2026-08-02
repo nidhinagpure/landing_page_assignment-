6ba6db08a27a757bfb91abe3143bf6410e910e22
nidhinagpure <nagpurenidhi8@gmail.com>
2026-08-03 01:05:25 +0530

    added header

--- script/schoolslider.js
+++ script/schoolslider.js
@@ -28,3 +28,15 @@ const swriper = new initSwiper(".exhibitionSwiper",{
         1400:{slidesPerView:4}
     }
 });
+
+/* Header */
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
